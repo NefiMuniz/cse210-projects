91835a91ca0885bd196537196bc8d4c5200be0ab
nefims <nefims@gmail.com>
2024-06-26 21:32:55 -0300

    Utility adjustments

--- prove/Develop02/Utility.cs
+++ prove/Develop02/Utility.cs
@@ -6,9 +6,18 @@ public static class Utility
     private static List<string> _prompts = new List<string>
     {
         "What are you grateful for today?",
-        "What is your biggest goal?",
-        "Describe a happy moment in your life.",
-        "What is your favorite hobby?"
+        "Did you serve someone today?",
+        "Describe a happy moment in your day.",
+        "How did you help someone today?",
+        "Did you learn anything new today?",
+        "What did you learn from the scriptures today?",
+        "How did you feel while praying today?",
+        "How did you overcome a difficult situation today?",
+        "What made you smile today?",
+        "What is one thing you would like to improve about today?",
+        "How did you show kindness to others today?",
+        "What new idea or concept did you encounter today?",
+        "How did you connect with your family or friends today?"
     };
 
     public static string GetRandomPrompt()
